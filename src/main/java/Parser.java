f4001926cd64ffa222363b440061ad88f49222d1
Jayden Chan <jaydencn7@gmail.com>
2018-03-03 01:41:55 -0800

    Add message, word, and character parsing methods

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -6,9 +6,7 @@
  * Parses a given conversation thread.
  */
 
-import java.util.Scanner;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
 import java.util.regex.*;
 
 import java.io.File;
@@ -20,19 +18,34 @@ public class Parser {
     private String title;
     private String user;
     private ArrayList<String> participants;
+    private ArrayList<String> messages;
+    private Map<String, Integer> messageMap = new HashMap<>();
 
     private int totalMessages;
+    private int wordCount;
+    private int charCount;
+    private double wordsPerMessage;
+    private double charsPerMessage;
 
     public Parser(String fileName) {
-        threadString = parseThreadToString(fileName).replaceAll("&#039;", "'");
-        title        = parseTitle();
-        participants = parseParticipants();
-        user         = parseUser();
+        String threadStringTemp = parseThreadToString(fileName);
+        threadString            = threadStringTemp.replaceAll("&#039;", "'");
+        threadString            = threadStringTemp.replaceAll("&quot;", "\"");
+
+        title                   = parseTitle();
+        participants            = parseParticipants();
+        user                    = parseUser();
+        initMessageMap();
+        messages                = parseMessages();
+        totalMessages           = messages.size();
+        computeWordAndCharCount();
+        wordsPerMessage         = (double)wordCount / (double)totalMessages;
+        charsPerMessage         = (double)charCount / (double)totalMessages;
     }
 
-    /****************************************************************/
-    /*                        Getter methods                        */
-    /****************************************************************/
+/****************************************************************/
+/*                        Getter methods                        */
+/****************************************************************/
 
     public String getTitle() {
         return title;
@@ -46,9 +59,63 @@ public class Parser {
         return participants;
     }
 
-    /****************************************************************/
-    /*                    Private helper methods                    */
-    /****************************************************************/
+/****************************************************************/
+/*                    Private helper methods                    */
+/****************************************************************/
+
+    private void computeWordAndCharCount() {
+        for(String s : messages) {
+            wordCount += wordCount(s);
+            charCount += s.length();
+        }
+    }
+
+    private int wordCount(String str) {
+        return str.split("\\s+").length;
+    }
+
+    private ArrayList<String> parseMessages() {
+        ArrayList<String> toReturn = new ArrayList<>();
+
+        Pattern messagePattern = Pattern.compile("(?<=<p>)(.*?)(?=<)");
+        Matcher messageMatcher = messagePattern.matcher(threadString);
+
+        Pattern senderPattern = Pattern.compile("(?<=\"user\">)(.*?)(?=<)");
+        Matcher senderMatcher = senderPattern.matcher(threadString);
+
+        while(messageMatcher.find()) {
+            toReturn.add(messageMatcher.group());
+
+            if(senderMatcher.find()) {
+                addMessageToUser(senderMatcher.group());
+            }
+        }
+
+        return toReturn;
+    }
+
+    private void addMessageToUser(String user) {
+        for(String s : participants) {
+            if(s.equals(user)) {
+                int prev = messageMap.get(s);
+                messageMap.replace(s, prev + 1);
+                return;
+            }
+        }
+
+        if(user.equals(this.user)) {
+            int prev = messageMap.get(user);
+            messageMap.replace(user, prev + 1);
+        }
+    }
+
+    private void initMessageMap() {
+        for(String s : participants) {
+            messageMap.put(s, 0);
+        }
+
+        messageMap.put(user, 0);
+    }
 
     private String parseThreadToString(String file) throws IllegalArgumentException {
         Scanner sc;
@@ -130,24 +197,44 @@ public class Parser {
         return null;
     }
 
-    /****************************************************************/
-    /*                           Override                           */
-    /****************************************************************/
+/****************************************************************/
+/*                           Override                           */
+/****************************************************************/
 
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
         sb.append("Title: ");
         sb.append(title+"\n");
-        sb.append("Self: " + user);
-        sb.append("\nParticipants: ");
+        sb.append("Self: " + user + "\n");
+        sb.append("Participants: ");
 
         for(String p : participants) {
             sb.append(p);
             sb.append(", ");
         }
 
-        sb.setLength(sb.length() - 2);
+        sb.deleteCharAt(sb.length() - 1);
+        sb.deleteCharAt(sb.length() - 1);
+
+        sb.append("\n\nMessage breakdown:\n");
+
+        for(String s : participants) {
+            int currMessages = messageMap.get(s);
+            sb.append(s + ": ");
+            sb.append(currMessages);
+            sb.append(String.format(" (%-3.1f%%)", ((double)currMessages / (double)totalMessages) * 100));
+            sb.append("\n");
+        }
+
+        sb.append(user + ": ");
+        sb.append(messageMap.get(user));
+        sb.append(String.format(" (%-3.1f%%)\n\n", ((double)messageMap.get(user) / (double)totalMessages) * 100));
+        sb.append("Total messages:    " + totalMessages + "\n");
+        sb.append("Total words:       " + wordCount + "\n");
+        sb.append("Words per message: " + wordsPerMessage + "\n");
+        sb.append("Chars per message: " + charsPerMessage + "\n");
+
         return sb.toString();
     }
 }
